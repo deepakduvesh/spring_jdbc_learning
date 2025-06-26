f8953b350ca0809d15fd4ffc26716eb22a5609f1
deepakduvesh <121632554+deepakduvesh@users.noreply.github.com>
2025-06-26 09:45:53 +0530

    java configuration without xml

--- springjdbc/src/main/java/com/spring/jdbc/App.java
+++ springjdbc/src/main/java/com/spring/jdbc/App.java
@@ -2,6 +2,7 @@ package com.spring.jdbc;
 import java.util.List;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -11,7 +12,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        		ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+    			ApplicationContext context = new AnnotationConfigApplicationContext(JdbcConfig.class);
+//        		ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
 //        		JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
         		
         		//insert query
